11d9bf6697d318b9cc06885519c94b453cf75687
Toni Saade <tonisaade2013@gmail.com>
2023-02-16 11:05:38 -0300

    Comentarios añadidos en for.js

--- Est_Programación_Javascript/for.js
+++ Est_Programación_Javascript/for.js
@@ -2,7 +2,7 @@
 var x = 0;
 
 for (x; x <= 5; x++) {
-    console.log ("El valor de x es:" +x); 
+    console.log ("El valor de x es:" +x); //sentencia que se va a ejecutar hasta cumplir la condición
 }
 
 //ingresamos la variable x con el valor asignado igual a 0.
@@ -10,6 +10,8 @@ for (x; x <= 5; x++) {
 //colocamos la sentencia for, que es la que inicia el bucle o iteración que consiste en las tres 
 //expresiones opcionales indicadas anteriormente.
 
-//x++ es la sentencia que incrementa o decrementa la variable según el signo que pongamos, cada
+//x++ es la expresión que incrementa o decrementa la variable según el signo que pongamos, cada
 //incremento de la variable va a ser de una unidad (+1) hasta que se cumpla la condición que 
-//habíamos puesto antes, en este caso, que x sea menor o igual a 5.
+//habíamos puesto antes, en este caso, que x sea menor o igual a 5. Como se ve en el navegador, lo que
+//esto hace es repetir la sentencia colocada abajo hasta que se cumpla la condición de que x sea menor
+//o igual a 5.
