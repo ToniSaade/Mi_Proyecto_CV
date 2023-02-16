658788b817e86f0e364b050bd1d342e8ff0dad72
Toni Saade <tonisaade2013@gmail.com>
2023-02-16 11:00:13 -0300

    Guardar for.js

---
+++ Est_Programación_Javascript/for.js
@@ -0,0 +1,15 @@
+// for (inicialización; condición; incremento/decremento)
+var x = 0;
+
+for (x; x <= 5; x++) {
+    console.log ("El valor de x es:" +x); 
+}
+
+//ingresamos la variable x con el valor asignado igual a 0.
+
+//colocamos la sentencia for, que es la que inicia el bucle o iteración que consiste en las tres 
+//expresiones opcionales indicadas anteriormente.
+
+//x++ es la sentencia que incrementa o decrementa la variable según el signo que pongamos, cada
+//incremento de la variable va a ser de una unidad (+1) hasta que se cumpla la condición que 
+//habíamos puesto antes, en este caso, que x sea menor o igual a 5.
