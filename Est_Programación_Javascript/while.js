fccbdc9ace0d0f58fa98e2b48ba0985a03fbb6a9
Toni Saade <tonisaade2013@gmail.com>
2023-02-16 15:17:24 -0300

    Guardar while.js

---
+++ Est_Programación_Javascript/while.js
@@ -0,0 +1,11 @@
+//El iterador while se usa para ejecutar un bloque de código mientras una condición sea
+//verdadera. Ejemplo:
+
+var x = 10;
+
+while (x > 0) {
+    console.log ("Mi variable vale:"+x);
+    x--;
+}
+
+//Este iterador lo usaremos cuando no sepamos la cantidad de veces que se repetirá el bucle.
